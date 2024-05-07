c43ff8d8e6bd90719f65b3add46ecd8815ee1691
Saurav Kamble <sauravkamble.dev@gmail.com>
2024-05-07 09:39:53 +0530

    add decodedToken

--- src/helpers/dataFromToken.ts
+++ src/helpers/dataFromToken.ts
@@ -6,7 +6,6 @@ export const dataFromToken = (request: NextRequest) => {
     const token = request.cookies.get("token")?.value || "";
     const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
     return decodedToken.id;
-    
   } catch (error: any) {
     throw new Error(error.message);
   }
