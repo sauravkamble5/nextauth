946bd57f2a4f44d9a2a77af37c33a4bec9712f81
Saurav Kamble <sauravkamble.dev@gmail.com>
2024-05-13 12:16:22 +0530

    add verify email page

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-const VerifyEmail = () => {
+export default function VerifyEmailPage() {
+  const [token, setToken] = useState("");
+  const [verified, setVerified] = useState(false);
+  const [error, setError] = useState(false);
+
+  const verifyUserEmail = async () => {
+    try {
+      await axios.post("/api/users/verifyemail", { token });
+      setVerified(true);
+      setError(false);
+    } catch (error: any) {
+      setError(true);
+      console.log(error.response.data);
+    }
+  };
+
+  useEffect(() => {
+    setError(false);
+    const urlToken = window.location.search.split("=")[1];
+    setToken(urlToken || "");
+  }, []);
+
+  useEffect(() => {
+    setError(false);
+    if (token.length > 0) {
+      verifyUserEmail();
+    }
+  }, [token]);
   return (
-    <div>page</div>
-  )
+    <div className="bg-amber-400 flex flex-col items-center justify-center min-h-screen py-2 ">
+      <h1 className="bg-lime-200 text-4xl">Verify Email</h1>
+      <h2 className="bg-orange-500 text-black p-2 ">
+        {token ? `${token}` : "No token"}{" "}
+      </h2>
+      {verified && (
+        <div>
+          <h2>Verified</h2>
+          <Link href="/login">Login </Link>
+        </div>
+      )}
+      {error && (
+        <div>
+          <h2>Error</h2>
+        </div>
+      )}
+    </div>
+  );
 }
-
-export default VerifyEmail
