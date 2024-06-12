c3f5ae1633908fab66a0a3493a0d152d75fd226c
sauravkamble5 <sauravkamble.dev@gmail.com>
2024-06-12 21:31:36 +0530

    add check-username-unique  +  verify-code

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,75 @@
+import dbConnect from '@/lib/dbConnect';
+import UserModel from '@/model/User';
+import { usernameValidation } from '@/schemas/signUpSchema';
+import { z } from 'zod';
+
+const UsernameQuerySchema = z.object({
+	username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+	if (request.method !== 'GET') {
+		return Response.json(
+			{
+				success: false,
+				message: 'Only GET method allowed',
+			},
+			{ status: 405 }
+		);
+	}
+	await dbConnect();
+
+	try {
+		const { searchParams } = new URL(request.url);
+		const queryParams = {
+			username: searchParams.get('username'),
+		};
+
+		//validate withh zod
+		const result = UsernameQuerySchema.safeParse(queryParams);
+
+		console.log(result);
+
+		if (!result.success) {
+			const usernameErrors = result.error.format().username?._errors || [];
+			return Response.json(
+				{
+					success: false,
+					message:
+						usernameErrors?.length > 0
+							? usernameErrors.join(', ')
+							: 'Invalid query parameters',
+				},
+				{ status: 400 }
+			);
+		}
+		const { username } = result.data;
+    
+		const existingVerifiedUser = await UserModel.findOne({
+			username,
+			isVerified: true,
+		});
+		if (existingVerifiedUser) {
+			return Response.json(
+				{
+					success: false,
+					message: 'Username already taken',
+				},
+				{ status: 400 }
+			);
+		}
+		return Response.json(
+			{
+				success: true,
+				message: 'Username is unique',
+			},
+			{ status: 200 }
+		);
+	} catch (error) {
+		console.error('Error checking username', error);
+		return Response.json(
+			{ success: false, message: 'Error checking username' },
+			{ status: 500 }
+		);
+	}
+}
--- src/helpers/sendVerificatoinEmail.ts
+++ src/helpers/sendVerificatoinEmail.ts
@@ -15,7 +15,7 @@ export async function sendVerificationEmail(
 			react: VerificationEmail({ username, otp: verifyCode }),
 		});
 
-		return { success: true, message: 'Verification email send successfully' };
+		return { success: true, message: 'Verification email sent successfully' };
 	} catch (emailError) {
 		console.log('Error sending verification email', emailError);
 		return { success: false, message: 'Failed to send verification email' };
