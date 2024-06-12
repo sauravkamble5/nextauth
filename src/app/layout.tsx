135db3d7dc4e8aea8a43a73853464f0cb8d34357
sauravkamble5 <sauravkamble.dev@gmail.com>
2024-06-12 16:52:35 +0530

    add next-auth + middleware + AuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Outfit } from 'next/font/google';
 import './globals.css';
+import AuthProvider from '@/context/AuthProvider';
 
 const outfit = Outfit({ subsets: ['latin'] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={outfit.className}>{children}</body>
+			<AuthProvider>
+				<body className={outfit.className}>{children}</body>
+			</AuthProvider>
 		</html>
 	);
 }
